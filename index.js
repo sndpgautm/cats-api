65efb6020351a8ae3d8cf8ca5be239194f9591ef
Sandip Gautam <rockersandip96@gmail.com>
2019-03-20 14:38:14 +0200

    Did some refactoring

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const app = express();
+require('dotenv').config();
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+const express = require('express');
+const app = express();
+
 
 var catSchema = new Schema({
     name: String,
@@ -13,9 +16,9 @@ var catSchema = new Schema({
 
 var Cat = mongoose.model('Cat', catSchema);
 
-mongoose.connect('mongodb://myTester:xyz123@localhost:27017/test', {useNewUrlParser: true}).then(() => {
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`, {useNewUrlParser: true}).then(() => {
     console.log('Connected successfully.');
-    app.listen(3000, ()=> console.log('Listening on port 3000...'));
+    app.listen(process.env.APP_PORT, ()=> console.log(`Listening on port...${process.env.APP_PORT}`));
 },err => {
     console.log('Connection to db failed: ' + err);
 });
