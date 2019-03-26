2f22d5efd9475d769dd6894124586681a28a69ef
Sandip Gautam <rockersandip96@gmail.com>
2019-03-26 11:30:55 +0200

    updated server.js

--- server.js
+++ server.js
@@ -7,20 +7,31 @@ const bodyParser = require('body-parser');
 const postRoute = require('./modules/postRoute.js');
 const routes = require('./modules/routes.js');
 //Parse Application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 //Parse application/JSON
 app.use(bodyParser.json());
 
 //Connects to database
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${process.env.DB_HOST}:${process.env.DB_PORT}/test`, {useNewUrlParser: true}).then(() => {
-    console.log('Connected successfully.');
-    app.listen(process.env.APP_PORT, () => console.log(`Listening on port...${process.env.APP_PORT}`));
-},err => {
-    console.log('Connection to db failed: ' + err);
-});
+mongoose
+  .connect(
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PWD}@${
+      process.env.DB_HOST
+    }:${process.env.DB_PORT}/demo`,
+    { useNewUrlParser: true }
+  )
+  .then(
+    () => {
+      console.log('Connected successfully.');
+      app.listen(process.env.APP_PORT, () =>
+        console.log(`Listening on port...${process.env.APP_PORT}`)
+      );
+    },
+    err => {
+      console.log('Connection to db failed: ' + err);
+    }
+  );
 
-
-//Handle routing 
+//Handle routing
 routes(app);
 //Using PostRoute to post the data into db from the form
 postRoute(app);
