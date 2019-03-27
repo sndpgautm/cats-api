fccbacfbed8a84bf2b2378c50e9e129e90a9331f
Sandip Gautam <rockersandip96@gmail.com>
2019-03-27 10:48:03 +0200

    tetsing local passport authentication

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+//For jelastic ssl use
 app.enable('trust proxy');
-
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 //Importing custom modules
 const postRoute = require('./modules/postRoute.js');
 const routes = require('./modules/routes.js');
 //Parse Application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 //Parse application/JSON
 app.use(bodyParser.json());
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+//Passport Authentication with local username and password
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    if (
+      username !== process.env.username ||
+      password !== process.env.password
+    ) {
+      done(null, false, { message: 'Incorrect credentials.' });
+      return;
+    }
+    return done(null, { user: username }); // returned object usally contains something to identify the user.. returning password would be stupid
+  })
+);
+app.use(passport.initialize());
 
 //Connects to database
 mongoose
@@ -34,6 +50,8 @@ mongoose
     }
   );
 
+//For local dev disabling https conenction
+//Only enable when delpolying to git and jelastic
 app.use((req, res, next) => {
   if (req.secure) {
     // request was via https, so do no special handling
@@ -42,6 +60,19 @@ app.use((req, res, next) => {
     // request was via http, so redirect to https
     res.redirect('https://' + req.headers.host + req.url);
   }
+});
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/test',
+    session: false
+  })
+);
+
+app.get('/test', (req, res) => {
+  res.send('login fail');
 });
 
 //Handle routing
